460c0dfe5ddc610709e7923a40ba78cf2230e2fa
Jozef Tarfa <tarfajozef@gmail.com>
2024-10-09 09:58:34 +0200

    Add 03-du.txt

--- 04-cyklus/03-faktorial.py
+++ 04-cyklus/03-faktorial.py
@@ -2,5 +2,5 @@ n = int(input ("Zadaj N: "))
 faktorial = 1
 for i in range(n): 
     faktorial = faktorial * (i+1)
-# print(n, "!=", faktorial)
+    # print(n, "!=", faktorial)
 print(f"{n}! = {faktorial}") # {} napíšeme pravý alt+bn {} 
