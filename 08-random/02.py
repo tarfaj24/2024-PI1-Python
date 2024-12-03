6f9b54a791d899b82e755320965a6779d3006da1
Jozef Tarfa <tarfajozef@gmail.com>
2024-12-03 14:11:53 +0100

    add-1,2,3,4

--- 08-random/02.py
+++ 08-random/02.py
@@ -9,8 +9,8 @@ pocet = int(input("zadaj pocet stvorcov: "))
 canvas.pack()
 
 for i in range(pocet):
-    x = random.randint(3,487-dlzka-3)
-    y = random.randint(3,387-dlzka-3)
+    x = random.randint(3,500-dlzka-3)
+    y = random.randint(3,400-dlzka-3)
     canvas.create_rectangle(x,y,x+dlzka,y+dlzka,fill=random.choice(["red","green","blue","yellow"]))
 
-tkinter.mainloop()
+tkinter.mainloop(
--- DU/canvasskuska.py
+++
@@ -1,5 +0,0 @@
-import tkinter
-canvas = tkinter.Canvas()
-canvas.pack()
-
-canvas.mainloop()
