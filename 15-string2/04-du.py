11d152982b7706d3e54dda20e1ed8cecb90856f6
Jozef Tarfa <tarfajozef@gmail.com>
2025-04-01 19:18:14 +0200

    add-stringy

--- 15-string2/04-du.py
+++ 15-string2/04-du.py
@@ -1,3 +1,38 @@
 #uzivatel zada celu vetu a program vetu rozdeli na slova, ktore vypise nahodnou farbou na nahonom mieste
 #aj pismenka zo slov vypise nahodnymi farbami
 #test zo stringov znakove retazce ako retazec.count, retazec.find ...
+import tkinter,random
+
+canvas = tkinter.Canvas(height=400,width = 400)
+canvas.pack()
+
+
+
+def program():
+    slovo = ""
+    
+    for znak in retazec.get():
+        slovo = slovo + znak
+        farba = f"#{random.randrange(256**3):06x}"
+
+        if znak == " ":
+            x = random.randint(30,370)
+            y = random.randint(30,370)
+            canvas.create_text(x,y,text= slovo,font="arial 15",fill = farba)
+            
+            slovo = ""
+        else:
+            print("teraz nie")
+        x = random.randint(30,370)
+        y = random.randint(30,370)
+    canvas.create_text(x,y,text= slovo,font="arial 15",fill = farba)
+        
+tkinter.Label(text="zadaj vetu").pack()
+retazec = tkinter.Entry()
+retazec.pack()
+tkinter.Button(text = "spusti program",command = program).pack()
+
+
+
+
+tkinter.mainloop()
---
+++ 15-string2/du.py
@@ -0,0 +1,26 @@
+import tkinter, random
+
+canvas = tkinter.Canvas(dlzka=400, sirka=400)
+canvas.pack()
+
+def program():
+    canvas.delete("all")
+    x = random.randint(30,370)
+    y = random.randint(30,370)
+
+    for znak in retazec.get():
+        farba = f"#{random.randrange(256**3):06x}"
+        canvas.create_text(x, y, text=znak, font="arial 15", fill=farba)
+        x += 12
+        
+        if znak == " ":
+
+            x = random.randint(30,370)
+            y = random.randint(30,370)
+
+tkinter.Label(text="zadaj vetu:").pack()
+retazec = tkinter.Entry()
+retazec.pack()
+tkinter.Button(text="spusti program", command=program).pack()
+
+tkinter.mainloop()
