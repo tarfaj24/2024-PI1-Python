5b64f2a1d2759ffe395b84a843bfa63e20761d9a
tarfaj24 <tarfajozef@gmail.com>
2025-06-18 09:15:32 +0200

    add-wheelspin-3

---
+++ wheel_spin/globalne-skusanie.py
@@ -0,0 +1,16 @@
+x = 1
+def f():
+    y = 2
+    print(x)
+    def d():
+        print(y)
+    d()
+    print(x)
+f()
+
+
+# x = 1
+# def f():
+#     print(x)
+# f()
+
--- wheel_spin/wheelspin.py
+++ wheel_spin/wheelspin.py
@@ -1,24 +1,25 @@
 import tkinter as tk
 import random
 
-spustene = True
+
 
 argument_uhol1 =random.randint(0,360)
 
+argument_spustene = True
 
 input = input("vypis mena")
 inputsplitnuty = input.split()
 
-dlzkainputu = len(inputsplitnuty)
+print(inputsplitnuty)
 
-rychlost_kolesa = 16
+dlzkainputu_premenna = len(inputsplitnuty)
 
-spomalovacia_premenna1 = 13.0
+
+argument_spomalovacia_premenna1 = 13.0
 spomalovacia_premenna = 7.0
 
-zvacsovanie = 360/dlzkainputu
-tisicka = 1000
-pocet_vykresleni= 0
+zvacsovanie_premenna = 360/dlzkainputu_premenna
+pocet_vykresleni_premenna = 0
 
 def _delete_():
     platno.delete("all")
@@ -34,20 +35,14 @@ def tick(sekunda):
 #     uhol -=30
 #     print(uhol)
 
-def koleso(uhol):
+def koleso(uhol,zvacsovanie,dlzkainputu,spomalovacia_premenna1,spomalovacia_premenna,spustene,pocet_vykresleni):
     
-    global zvacsovanie
-    global dlzkainputu
-    global rychlost_kolesa
-    global tisicka
-    global pocet_vykresleni
-    global spustene
-    global spomalovacia_premenna
-    global spomalovacia_premenna1
+    
+    
 
     if spustene == True:
         _delete_()
-        platno.after(rychlost_kolesa,lambda: koleso(uhol))
+        platno.after(16,lambda: koleso(uhol,zvacsovanie,dlzkainputu,spomalovacia_premenna1,spomalovacia_premenna,spustene,pocet_vykresleni))
         zvacsovanie=0
         for i in range(dlzkainputu):
             platno.create_arc(100,100,300,300,start = 20 + zvacsovanie + uhol,extent = 360/dlzkainputu)
@@ -72,7 +67,7 @@ def koleso(uhol):
             spomalovacia_premenna1-=0.02
             uhol+=spomalovacia_premenna1
 
-        print(uhol)
+        
 
 
     
@@ -83,7 +78,8 @@ platno = tk.Canvas(okno,height = 400,width = 400)
 platno.pack()
 
 
-koleso(argument_uhol1)
+
+koleso(argument_uhol1,zvacsovanie_premenna,dlzkainputu_premenna,argument_spomalovacia_premenna1,spomalovacia_premenna,argument_spustene,pocet_vykresleni_premenna)
 
 
 
